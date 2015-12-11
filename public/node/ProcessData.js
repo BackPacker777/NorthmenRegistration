5bc8a9914059791063ab606e2e18c7479794c79f
backpacker777 <Baraddur1>
2015-12-11 15:09:06 -0500

    ??

---
+++ public/node/ProcessData.js
@@ -0,0 +1,14 @@
+/*  AUTHOR: hbates@northmen.org
+ *  VERSION: 1.10
+ *  CREATED: 11.25.2015
+ *  PURPOSE: Register Northmen!
+ */
+
+"use strict";
+
+export default class ProcessData {
+    constructor() {
+
+    }
+
+}
---
+++ public/node/main.js
@@ -0,0 +1,19 @@
+/*  AUTHOR: hbates@northmen.org
+ *  VERSION: 1.10
+ *  CREATED: 11.25.2015
+ *  PURPOSE: Register Northmen!
+ */
+
+"use strict";
+
+import ProcessData from './ProcessData';
+
+class main {
+    constructor() {
+
+    }
+
+
+}
+
+new main();
